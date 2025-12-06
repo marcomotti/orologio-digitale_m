f62aaa58afcc82aac4c98a950886f7a5f0cf23e4
marcomotti <50266566+marcomotti@users.noreply.github.com>
2025-12-06 10:23:53 +0100

    Add light strip configuration to DigitClock

--- components/digit_clock/__init__.py
+++ components/digit_clock/__init__.py
@@ -1,12 +1,15 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.components import light
 
 digit_clock_ns = cg.esphome_ns.namespace("digit_clock")
 DigitClock = digit_clock_ns.class_("DigitClock", cg.Component)
 
-CONFIG_SCHEMA = cv.Schema({})
+CONFIG_SCHEMA = cv.Schema({
+    cv.Required("strip"): cv.use_id(light.AddressableLight)
+})
 
 async def to_code(config):
-    pass
-
-
+    strip = await cg.get_variable(config["strip"])
+    var = cg.new_Pvariable("digitclock", DigitClock(strip))
+    await cg.register_component(var, config)
