3536c6a1ff986943ade9428b7852974574b8537f
marcomotti <50266566+marcomotti@users.noreply.github.com>
2025-12-06 10:08:22 +0100

    Initialize digit_clock component with basic schema

--- components/digit_clock/__init__.py
+++ components/digit_clock/__init__.py
@@ -1,1 +1,12 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+
+digit_clock_ns = cg.esphome_ns.namespace("digit_clock")
+DigitClock = digit_clock_ns.class_("DigitClock", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({})
+
+async def to_code(config):
+    var = cg.new_Pvariable("digitclock", DigitClock())
+    await cg.register_component(var, config)
 
